430ee3fc5fb80eee1fe22aa70c772568402f2017
Unix One <unix1@live.com>
2018-02-24 17:55:44 -0800

    yann_layout_server uses yann_layout:layout() type

--- src/yann_layout_server.erl
+++ src/yann_layout_server.erl
@@ -10,6 +10,7 @@
 %% - stores network layout (layers and neurons per layer)
 %% - stores neuron address (layer and index) mapping to its pid
 %% - monitors neuron pids and removes them from mapping when they crash
+%% - adds new neuron pids to the mapping when assigning them network addresses
 %% @end
 %%%-------------------------------------------------------------------
 
@@ -26,10 +27,7 @@
 % Behavior callbacks
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
--type state() :: #{layout => layout(), neuron_map => neuron_map()}.
--type layout() :: #{layers => [layer()]}.
--type layer() :: #{type => layer_type(), number_of_neurons => integer()}.
--type layer_type() :: input | hidden | output.
+-type state() :: #{layout => yann_layout:layout(), neuron_map => neuron_map()}.
 -type neuron_map() :: [layer_neuron_map()].
 -type layer_neuron_map() :: [pid()].
 
@@ -49,13 +47,13 @@ start_link() ->
 
 -spec init([]) -> {ok, state()}.
 init([]) ->
-    State = #{network_map => #{layers => []}, neuron_map => []},
+    State = #{layout => yann_layout:new([]), neuron_map => []},
     {ok, State}.
 
 %%====================================================================
 %% API
 %%====================================================================
--spec set_layout(Layout :: layout()) -> ok.
+-spec set_layout(Layout :: yann_layout:layout()) -> ok.
 set_layout(_Layout) ->
     ok.
 
